b3242f5688da73c6c6fa026037a67ce4c73bb2fa
Archana <archanapatukuri@gmail.com>
2022-08-22 14:16:36 +0530

    Add files via upload

---
+++ aroma.js
@@ -0,0 +1,18 @@
+function dap() {
+    alert("Watsapp your requirements to '951559417'");
+}
+function dapFinal(){
+	alert("Watsapp your requirements to '951559417'");
+}
+
+function linkedin() {
+    window.location.href = 'https://www.linkedin.com/in/archana-patukuri-495055162/';
+}
+
+function facebook() {
+    window.location.href = 'https://www.facebook.com/archana.patukuri.9';
+}
+
+function instagram() {
+    window.location.href = 'https://www.instagram.com/archana_patukuri/';
+}
